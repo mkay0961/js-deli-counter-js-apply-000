0aa69f138a28a565d6e096cc7c7eaea53b1e1490
Matthew Kay <mkay0961@gmail.com>
2018-12-10 14:06:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,23 +2,11 @@ function takeANumber(line, name){
   line.push(name);
   var numberInLine = line.length;
   var message = "Welcome, " + name + ". You are number " +numberInLine + " in line.";
-  // if(numberInLine == 1){
-  //   message += "1st in line.";
-  // }
-  // if(numberInLine == 2){
-  //   message += "2nd in line.";
-  // } 
-  // if(numberInLine == 3){
-  //   message += "3rd in line.";
-  // }
-  // if(numberInLine > 3){
-  //   message += numberInLine+ "th in line.";
-  // }
   return message
 }
 
 function nowServing(katzDeliLine){
-  if(katzDeliLine.length== 0){
+  if(katzDeliLine.length == 0){
     return "There is nobody waiting to be served!"
   }else{
     var name = katzDeliLine[0];
